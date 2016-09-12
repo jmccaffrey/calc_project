2b2dd5c596104d1b9fe526713ad2597a0e18df6b
John McCaffrey <john.mccaffrey@gmail.com>
2016-09-12 07:50:15 +0000

    initial commit

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,17 @@
+require 'calculator'
+
+RSpec.describe Calculator do
+  
+  describe "#add" do
+   let(:calc) { calc = Calculator.new }
+    it "adds 2 and 2" do
+      expect(calc.add(2, 2)).to eq(4)
+    end
+    
+    it "handles negative numbers" do
+	   expect(calc.add(5, -2)).to eq(3)
+    end
+    
+  end
+  
+end
